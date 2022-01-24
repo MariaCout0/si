f5109acd8d804c9cccef99a55efe129830a4ca21
Maria Couto <74066192+MariaCout0@users.noreply.github.com>
2022-01-24 23:10:24 +0000

    Update knn.py

--- src/si/supervised/knn.py
+++ src/si/supervised/knn.py
@@ -6,7 +6,7 @@ from si.util.metrics import accuracy_score
 
 class KNN():
     def __init__(self, num_n, classification = True):
-        super(KNN).__init__()    # instanciar a flag para saber se foi feito o fit ao modelo ou não
+        super(KNN).__init__()    # to know if the model was fitted or not
         self.k_n = num_n
         self.classification = classification
 
@@ -15,24 +15,21 @@ class KNN():
         self.is_fitted = True
     
     def get_neighbors(self, x):
-        #idxs = np.apply_along_axis(self.dataset.X, axis=0, arr=self.dataset.X)
-        distances = l2_distance(x, self.dataset.X) # calcular as distâncias de X a todos os outros pontos do dataset usando l2 - euclidian distance
-        sorted_index = np.argsort(distances)      # ordenar para ter os índices que correspondem às melhores distâncias
-        # ordenar os índices por ordem crescente de distância
-        return sorted_index[:self.k_n]   # selecionar os knn
+        distances = l2_distance(x, self.dataset.X) # calculate distances from X to all other points in the dataset 
+        sorted_index = np.argsort(distances)      # sort the indices that correspond to the best distances
+        return sorted_index[:self.k_n]   
 
     def predict(self, x):
         assert self.is_fitted, "Model must be fitted before predict"
         neighbors = self.get_neighbors(x)
-        values = self.dataset.Y[neighbors].tolist() # y_values= dataset.Y[máscara]
-        # os valores vão servir para fazer a votação (retornar aquele que aparece mais vezes) - isto para problema de classificação
+        values = self.dataset.Y[neighbors].tolist() 
+        # Classification
         if self.classification:
             prediction = max(set(values), key = values.count)
-        else:                                       # problema de regressão
+        else:                                       # Regression
             prediction = sum(values)/len(values)
         return prediction
 
     def cost(self):
-        # np.ma with mask
         y_pred = np.ma.apply_along_axis(self.predict,axis=0,arr=self.dataset.X.T)
         return accuracy_score(self.dataset.Y, y_pred)
